3782eaf63611294e06720777b4bef44fc765a703
Hovhannes Avagyan <hovhannes1894@gmail.com>
2018-10-08 16:39:50 -0400

    removed security vulnerabilities

--- controllers/router.js
+++ controllers/router.js
@@ -1,9 +1,6 @@
 var fs = require('fs');
 var atob = require('atob');
 var path = require('path');
-var ActiveDirectory = require('activedirectory');
-var jwt = require('jsonwebtoken');
-var _ = require('lodash');
 
 module.exports = function(app) {
     app.get('/', function(req, res, next) {
